014be6bc2266e06b6617e3f293e45459b7967c42
CloudstepperSS <jonas.haraldsson@outlook.com>
2026-05-17 22:52:56 +0000

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  SUPABASE_URL:      'https://gkukqcjusapgmqxbkdub.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrdWtxY2p1c2FwZ21xeGJrZHViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNDI4NDIsImV4cCI6MjA5NDYxODg0Mn0.bUDh70Z1gvCLyG3MMIMFjYHKliDlsi2JsOAmxBY-DPo',
-
-  // Paste your Cloudflare Worker URL here after deploying worker.js
-  // e.g. 'https://sneakervault.your-name.workers.dev'
-  WORKER_URL: 'sneakervault.jonas-haraldsson.workers.dev',
-};
